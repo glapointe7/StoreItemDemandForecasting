fc00776655139c4e918336fa03a963ba6d017f53
lapoig <dragongl@gmail.com>
2019-02-06 21:18:08 -0500

    Added normalization anomalies and table of contents. Added observations and introductions.

--- Anomalies.py
+++ Anomalies.py
@@ -5,21 +5,24 @@ from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
 from heapq import nlargest
 from random import randint
+from scipy import stats
 
 import matplotlib.pyplot as plt
 import pandas
+import statistics
+import numpy as np
 
 # Return the list of normals(1) or anomalies(-1) for each item of each store using the SVM algorithm 
 # where the data points are (x, y) = (date, sales).
 def ListAllByApplyingSVM(train, outliers_fraction):
     anomalies = []
-    for store in range(1, 11):
-        for item in range(1, 51):
-            store_item = train.loc[(train.store == store) & (train.item == item), ['date', 'sales']]
+    for store_id in range(1, 11):
+        for item_id in range(1, 51):
+            store_item = train.loc[(train.store == store_id) & (train.item == item_id), ['date', 'sales', 'month_number']]
             store_item.date = store_item.date.str.replace('\D', '').astype(int)
             
             scaler = StandardScaler()
-            np_scaled = scaler.fit_transform(store_item[['date', 'sales']])
+            np_scaled = scaler.fit_transform(store_item[['date', 'sales', 'month_number']])
             scaled_store_item = pandas.DataFrame(np_scaled)
             
             model = OneClassSVM(nu=outliers_fraction, 
@@ -33,13 +36,13 @@ def ListAllByApplyingSVM(train, outliers_fraction):
 # Return the list of normals(1) or anomalies(-1) for each item of each store using the isolation forest algorithm.
 def ListAllByApplyingIsolationForest(train, outliers_fraction):
     anomalies = []
-    for store in range(1, 11):
-        for item in range(1, 51):
-            store_item = train.loc[(train.store == store) & (train.item == item), ['date', 'sales']]
+    for store_id in range(1, 11):
+        for item_id in range(1, 51):
+            store_item = train.loc[(train.store == store_id) & (train.item == item_id), ['date', 'sales', 'month_number']]
             store_item.date = store_item.date.str.replace('\D', '').astype(int)
             
             scaler = StandardScaler()
-            np_scaled = scaler.fit_transform(store_item[['date', 'sales']])
+            np_scaled = scaler.fit_transform(store_item[['date', 'sales', 'month_number']])
             scaled_store_item = pandas.DataFrame(np_scaled)
             
             model =  IsolationForest(contamination=outliers_fraction)
@@ -49,10 +52,10 @@ def ListAllByApplyingIsolationForest(train, outliers_fraction):
 
 # Since the elbow curve is similar between all store items, we chose the first store and item to get the number of clusters.
 def GetKMeanNumberOfClusters(train, nb_clusters_to_test):
-    store_item = train.loc[(train.store == 1) & (train.item == 1), ['date', 'sales']]
+    store_item = train.loc[(train.store == 1) & (train.item == 1), ['date', 'sales', 'month_number']]
     store_item.date = store_item.date.str.replace('\D', '').astype(int)
     
-    kmeans = [KMeans(n_clusters=k).fit(store_item[['date', 'sales']]) for k in nb_clusters_to_test]
+    kmeans = [KMeans(n_clusters=k).fit(store_item[['date', 'sales', 'month_number']]) for k in nb_clusters_to_test]
     sum_squared_errors = [kmeans[k].inertia_ for k in range(len(kmeans))]
     
     return kmeans, sum_squared_errors
@@ -60,24 +63,45 @@ def GetKMeanNumberOfClusters(train, nb_clusters_to_test):
 # Return the list of normals(0) or anomalies(-1) for each item of each store using the K-Mean algorithm.
 def ListAllByApplyingKMean(train, outliers_fraction, cluster):
     anomalies = []
-    for store in range(1, 11):
-        for item in range(1, 51):
-            store_item = train.loc[(train.store == store) & (train.item == item), ['date', 'sales']]
+    for store_id in range(1, 11):
+        for item_id in range(1, 51):
+            store_item = train.loc[(train.store == store_id) & (train.item == item_id), ['date', 'sales', 'month_number']]
             store_item.date = store_item.date.str.replace('\D', '').astype(int)
             
-            distances = cdist(store_item[['date', 'sales']], cluster.cluster_centers_)
+            distances = cdist(store_item[['date', 'sales', 'month_number']], cluster.cluster_centers_)
             number_of_outliers = int(outliers_fraction * len(distances))
 
             distances = list(map(lambda x: max(x), distances))
             max_distances = nlargest(number_of_outliers, distances)
 
             threshold = max_distances[-1]
-            anomalies.extend(-((distances >= threshold).astype(int)))
+            anomalies.extend(-(distances >= threshold).astype(int))
     return anomalies
-            
 
+# Return the list of normals(0) or anomalies(-1) for each item of each store using the normalization method.
+def ListAllByApplyingNormalization(train, number_of_std, alpha):
+    normality = []
+    anomalies = []
+    for s in range(1, 11):
+        for i in range(1, 51):
+            item_by_year = train.loc[(train.store == s) & (train.item == i), ['date', 'sales', 'month_number']]
+            item_by_year.date = pandas.DatetimeIndex(item_by_year.date).year
+
+            for y in range(2013, 2018):
+                for m in range(1, 13):
+                    item_month = np.array(item_by_year.sales[(item_by_year.date == y) & (item_by_year.month_number == m)])
+
+                    w, p_value = stats.shapiro(item_month)
+                    normality.append(int(p_value > alpha))
+
+                    sales_by_month_mean = statistics.mean(item_month)
+                    sales_by_month_std = statistics.stdev(item_month)
+                    scores = np.absolute((item_month - sales_by_month_mean) / sales_by_month_std)
+                    anomalies.extend([-int(score > number_of_std) for score in scores])
+    return normality, anomalies
+    
 # Plot the anomalies detected in the sales of a random store item.
-# anomaly_type is any of 'svm', 'iforest', 'kmean'.
+# anomaly_type is any of 'svm', 'iforest', 'kmean', 'normal'.
 def PlotRandomStoreItem(train, anomaly_type):
     store_id = randint(1, 10)
     item_id = randint(1, 50)
